a68d3c1f5204bb78060dbfb34eb336ae93ea698a
GodVilan <srikanthreddynandireddy@gmail.com>
2024-12-08 15:19:08 -0600

    static

--- CECal/settings.py
+++ CECal/settings.py
@@ -110,19 +110,28 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Static files settings
+# STATIC_URL = '/static/'
+
+# # Location where static files will be collected (for production)
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# # Add the directories where Django will search for static files
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     BASE_DIR / "CarbonEmission/static",
+# ]
 STATIC_URL = '/static/'
 
-# Location where static files will be collected (for production)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# The location where the collected static files will be stored on Azure
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Add the directories where Django will search for static files
+# Add the directory containing your static files during development
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "CarbonEmission/static",
+    os.path.join(BASE_DIR, 'CarbonEmission', 'static'),
 ]
 
 
-SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
